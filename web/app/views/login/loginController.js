86b44c5dde1c0efc7f84f8bd234a0cd1fa0781a9
bsanchez12 <sanchezbriam@gmail.com>
2017-07-07 15:18:48 -0500

    cambio

--- web/app/views/login/loginController.js
+++ web/app/views/login/loginController.js
@@ -6,6 +6,8 @@ var req = {};
 app.controller("loginController", ['$http','$scope', '$rootScope', '$filter', '$location',
     function ($http, $scope, $rootScope, $filter, $location) {		
 
+		debugger;
+	
 		$scope.loginuser = function(){   
 			 req = {
              method: method,
